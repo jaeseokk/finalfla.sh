3c396deb74fd5dd7302873f5806353cde76c06d7
jaeseokk <kang89kr@gmail.com>
2020-12-04 00:13:24 +0900

    Enhance Loading component text
    * Add 'SOUND ON! VOLUME UP!'

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 
 import styles from './Loading.module.scss'
 
@@ -8,17 +8,32 @@ interface LoadingProps {
 }
 
 const Loading: React.FC<LoadingProps> = ({ ready, onClick }) => {
+  const [showSoundsOn, setShowSoundsOn] = useState(false)
   const handleClick = useCallback(() => {
     if (!ready) {
       return
     }
 
-    onClick()
+    setShowSoundsOn(true)
+
+    setTimeout(() => {
+      onClick()
+    }, 1000)
   }, [ready])
+
   return (
     <div className={styles.Loading} onClick={handleClick}>
       <div className={styles.content}></div>
-      <div className={styles.text}>{ready && <div>CLICK TO START</div>}</div>
+      <div className={styles.text}>
+        {ready &&
+          (showSoundsOn ? (
+            <div>
+              <span>SOUND ON!</span> <span>VOLUME UP!</span>
+            </div>
+          ) : (
+            <div>CLICK TO START</div>
+          ))}
+      </div>
     </div>
   )
 }
